af35ffd7bf42dd2d822d91a9cee174c29a0e53de
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-12 17:13:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,9 +8,9 @@ class Shoe
     @brand = brand  
   end 
   
-  def cobble
+  def cobble()
     puts "Your shoe is as good as new!"
-    @condition == "new"
+    self.condition == "new"
   end 
   
 end 
